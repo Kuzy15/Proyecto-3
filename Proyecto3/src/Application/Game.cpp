0f283f1e66dc07400c16faa14fa6b4c5286d5dca
Usuario Local <Usuario Local>
2018-03-07 17:56:17 +0100

    Comenzada inicialización de Ogre. Error 0xc000007b en Ogre

--- Proyecto3/src/Application/Game.cpp
+++ Proyecto3/src/Application/Game.cpp
@@ -1,8 +1,10 @@
 #include "Game.h"
 #include "Scenes.h"
+#include <iostream>
 
 
 
+#pragma region Constructor and destructor
  Game::Game(){
 
 	 b2Vec2 gravity(1.0f, 1.0f);
@@ -11,3 +13,59 @@
 
 
 }
+ Game::~Game(){
+
+
+ }
+#pragma endregion
+
+#pragma region Ogre Game functions
+void Game::initOgre(){
+
+	//Firstly we define wether we are using a resources and plugins file for debug
+	// or for release. Then we create the root.
+#ifdef _DEBUG
+	resCfgLoc = "OgreD/resources_d.cfg";
+	plugCfgLoc = "OgreD/plugins_d.cfg";
+#else
+	resCfgLoc = "Ogre/resources.cfg";
+	plugCfgLoc = "Ogre/plugins.cfg";
+#endif
+	root = new Ogre::Root(plugCfgLoc);
+
+	//------------------------------------------------------------------------------------------------------
+	//Setting UP Resources 
+
+	//Parsing the config file into the system.
+	cf.load(resCfgLoc);
+
+
+	//name: Path to resources in disk,
+	//loctype: defines what kind of location the element is (e.g. Filesystem, zip..)
+	Ogre::String name, locType;
+
+	//We now iterate through rach section in the resources.cfg.
+	//Sections are signaled as [NAME]
+	Ogre::ConfigFile::SectionIterator secIt = cf.getSectionIterator();
+	while (secIt.hasMoreElements())
+	{
+		Ogre::ConfigFile::SettingsMultiMap* settings = secIt.getNext();
+		Ogre::ConfigFile::SettingsMultiMap::iterator it;
+
+		//Now we are iterating INSIDE the section [secIt]
+		for (it = settings->begin(); it != settings->end(); ++it)
+		{
+			locType = it->first;
+			name = it->second;
+
+			//We now know the type of the element and its path.
+			//We add it as a location to the Resource Group Manager
+			Ogre::ResourceGroupManager::getSingleton().addResourceLocation(name, locType);
+			std::cout << "inserting:\n";
+			std::cout << name << locType << std::endl;
+		}
+	}
+
+
+ }
+#pragma endregion
--- Proyecto3/src/Application/Game.h
+++ Proyecto3/src/Application/Game.h
@@ -3,17 +3,29 @@
 
 #include <OgreRoot.h>
 #include <Box2D.h>
-
-
-
+#include <OgreConfigFile.h>
 
 class Game
 {
 public:
 	Game();
 	~Game();
+	void loop();
+
+
 
 private:
+
+	//Ogre parameters
+	Ogre::Root * root;
+	//Location and type of the resources and plugins files
+	Ogre::String resCfgLoc;
+	Ogre::String plugCfgLoc;
+	//configFile we are going to use to parse our configuration to the game
+	Ogre::ConfigFile cf;
+
+	
+	void initOgre();
 
 };
 
